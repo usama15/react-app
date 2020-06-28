0d0932ee4c1525567ed33b70b919bf97076cdf68
usama15 <princeusamaansari15@gmail.com>
2020-06-29 02:29:07 +0900

    upload data

---
+++ build/precache-manifest.4ed702093afd7761f0c5c571aeebb3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e680fd89b7607e2f65a96ba9a3b3d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f30f5421403b0c36dc5a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c05c8839468a275a366",
+    "url": "/static/js/2.fc383eab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc383eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30f5421403b0c36dc5a",
+    "url": "/static/js/main.24b3b792.chunk.js"
+  },
+  {
+    "revision": "77785b5d7f106e80a267",
+    "url": "/static/js/runtime-main.5bebf6b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
